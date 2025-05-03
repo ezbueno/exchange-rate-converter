ae605331708f0c35f0002b929f6dbfcff6a07c9f
ezbueno <ezandrobueno@yahoo.com.br>
2025-05-02 23:57:33 -0300

    Adjust CurrencyMenu to align with updated menu and enums

--- src/developer/ezandro/ui/CurrencyMenu.java
+++ src/developer/ezandro/ui/CurrencyMenu.java
@@ -1,20 +1,9 @@
 package developer.ezandro.ui;
 
+import developer.ezandro.utils.Message;
+
 public class CurrencyMenu {
     public void showMenu() {
-        System.out.print("""
-                ***********************************************
-                * Hello! Welcome to the Currency Converter!   *
-                *                                             *
-                * 1) Dollar ==> Argentine Peso                *
-                * 2) Argentine Peso ==> Dollar                *
-                * 3) Dollar ==> Brazilian Real                *
-                * 4) Brazilian Real ==> Dollar                *
-                * 5) Dollar ==> Colombian Peso                *
-                * 6) Colombian Peso ==> Dollar                *
-                * 7) Exit                                     *
-                ***********************************************
-                Please choose a valid option:
-                """);
+        System.out.print(Message.MENU_HEADER);
     }
 }
