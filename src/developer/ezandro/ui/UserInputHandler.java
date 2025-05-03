02a41abc287d50b50f6fa0a1bf8134ec294981a2
ezbueno <ezandrobueno@yahoo.com.br>
2025-05-02 23:57:33 -0300

    Enhance input validation and user feedback for option and amount inputs

--- src/developer/ezandro/ui/UserInputHandler.java
+++ src/developer/ezandro/ui/UserInputHandler.java
@@ -1,32 +1,54 @@
 package developer.ezandro.ui;
 
+import developer.ezandro.utils.CurrencyPairManager;
+import developer.ezandro.utils.Message;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class UserInputHandler {
     private UserInputHandler() {
     }
 
     public static int readValidOption(int option) {
-        return isValidOption(option) ? option : -1;
+        if (CurrencyPairManager.isValidOption(option)) {
+            return option;
+        }
+        throw new IllegalArgumentException(Message.INVALID_OPTION_MESSAGE);
     }
 
     public static void readValidAmount(double amount) {
         if (amount <= 0) {
-            throw new IllegalArgumentException("The entered value cannot be less than or equal to 0.");
+            throw new IllegalArgumentException(Message.AMOUNT_MUST_BE_POSITIVE);
         }
     }
 
-    private static boolean isValidOption(int option) {
-        switch (option) {
-            case 1,
-                 2,
-                 3,
-                 4,
-                 5,
-                 6,
-                 7 -> {
-                return true;
+    public static int readOption(Scanner scanner) {
+        while (true) {
+            try {
+                int option = scanner.nextInt();
+                return UserInputHandler.readValidOption(option);
+            } catch (InputMismatchException _) {
+                System.out.print(Message.INVALID_OPTION_MESSAGE);
+                scanner.nextLine();
+            } catch (IllegalArgumentException _) {
+                System.out.print(Message.INVALID_OPTION_MESSAGE);
             }
-            default -> {
-                return false;
+        }
+    }
+
+    public static double readAmount(Scanner scanner) {
+        while (true) {
+            try {
+                System.out.println(Message.ENTER_AMOUNT);
+                double amount = scanner.nextDouble();
+                UserInputHandler.readValidAmount(amount);
+                return amount;
+            } catch (InputMismatchException _) {
+                System.out.println(Message.INVALID_AMOUNT);
+                scanner.nextLine();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
             }
         }
     }
