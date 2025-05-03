ad84be4bc68cd3198edd0dfe99651e32087c7f82
ezbueno <ezandrobueno@yahoo.com.br>
2025-05-02 23:57:11 -0300

    Simplify result handling and encapsulate print logic in ConversionResult record

--- src/developer/ezandro/domain/ConversionResult.java
+++ src/developer/ezandro/domain/ConversionResult.java
@@ -1,4 +1,9 @@
 package developer.ezandro.domain;
 
+import developer.ezandro.utils.Message;
+
 public record ConversionResult(double conversion_result) {
+    public static void printConversionResult(double amount, String baseCurrency, double convertedAmount, String targetCurrency) {
+        System.out.printf(Message.CONVERSION_RESULT_MESSAGE, amount, baseCurrency, convertedAmount, targetCurrency);
+    }
 }
