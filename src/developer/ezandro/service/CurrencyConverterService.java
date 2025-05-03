aefa3d9e0c1ea6d13c2d87c43dd248ecfd54ea44
ezbueno <ezandrobueno@yahoo.com.br>
2025-05-02 23:57:11 -0300

    Refactor service to accept CurrencyCode enum and delegate API request

--- src/developer/ezandro/service/CurrencyConverterService.java
+++ src/developer/ezandro/service/CurrencyConverterService.java
@@ -1,4 +1,13 @@
 package developer.ezandro.service;
 
+import developer.ezandro.api.CurrencyExchangeClient;
+import developer.ezandro.domain.ConversionResult;
+
 public class CurrencyConverterService {
+    private CurrencyConverterService() {
+    }
+
+    public static ConversionResult convertCurrency(String base, String target, double amount) {
+        return CurrencyExchangeClient.getExchangeRateForCurrency(base, target, amount);
+    }
 }
